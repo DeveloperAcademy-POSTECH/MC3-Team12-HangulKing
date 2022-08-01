1cf99c420cf4aa4e708e3ed8c846bfc5383a8e8d
Seulki Lee <operry126@gmail.com>
2022-08-01 16:44:13 +0900

    style added gradient

--- HangulTop/ViewController/StudyMenuViewController.swift
+++ HangulTop/ViewController/StudyMenuViewController.swift
@@ -60,7 +60,7 @@ extension UIView{
         gradient.colors = [color1.cgColor,color2.cgColor]
         gradient.locations = [0.0 , 1.0]
         gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
-        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 0.0, y: 0.0)
         gradient.frame = bounds
         layer.addSublayer(gradient)
     }
